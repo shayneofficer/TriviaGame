fd81c72cd8784e3c81a9042636333050c69e63d0
shayneofficer <shayneofficer@gmail.com>
2018-11-18 14:24:06 -0600

    added question, answer content, and done button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,51 +41,83 @@ $("#start-button").on("click", function () {
 
 
     $.ajax({
-        url: "https://opentdb.com/api.php?amount=8&category=9&difficulty=easy&type=multiple",
+        url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        console.log(response.results.length);
 
         for (var i = 0; i < response.results.length; i++) {
 
             var answersArr = response.results[i].incorrect_answers;
             answersArr.push(response.results[i].correct_answer);
 
+            answersArr = shuffle(answersArr);
+
+
             var questionDiv = $("<div>");
-            questionDiv.attr("class", "row");
+            questionDiv.attr("class", "row question");
             questionDiv.html(`<div class="col">
             <h2>${response.results[i].question}</h2>
             <div class="form-check-inline">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="q1a1" value="option1">
-                <label class="form-check-label" for="q1a1">
-                    Option 1
+                <input class="form-check-input" type="radio" name="question${i + 1}" id="q${i + 1}a1" value="${answersArr[0]}">
+                <label class="form-check-label" for="q${i + 1}a1">
+                    ${answersArr[0]}
                 </label>
             </div>
             <div class="form-check-inline">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="q1a2" value="option2">
-                <label class="form-check-label" for="q1a2">
-                    Option 2
+                <input class="form-check-input" type="radio" name="question${i + 1}" id="q${i + 1}a2" value="${answersArr[1]}">
+                <label class="form-check-label" for="q${i + 1}a2">
+                    ${answersArr[1]}
                 </label>
             </div>
             <div class="form-check-inline">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="q1a3" value="option3">
-                <label class="form-check-label" for="q1a3">
-                    Option 2
+                <input class="form-check-input" type="radio" name="question${i + 1}" id="q${i + 1}a3" value="${answersArr[2]}">
+                <label class="form-check-label" for="q${i + 1}a3">
+                    ${answersArr[2]}
                 </label>
             </div>
             <div class="form-check-inline">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="q1a4" value="option3">
-                <label class="form-check-label" for="q1a4">
-                    Option 2
+                <input class="form-check-input" type="radio" name="question${i + 1}" id="q${i + 1}a4" value="${answersArr[3]}">
+                <label class="form-check-label" for="q${i + 1}a4">
+                    ${answersArr[3]}
                 </label>
             </div>
         </div>`)
 
             $(".container").append(questionDiv);
         }
+
+        var doneButton = $(`<div class="row" id="done-button-row">
+                                <div class="col">
+                                    <button type="button" id="done-button">
+                                        Done
+                                    </button>
+                                </div>
+                            </div>`);
+
+        $(".container").append(doneButton);
     })
 
     // timer.start();
 });
 
+
+// Fisher-Yates (aka Knuth) Shuffle
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
